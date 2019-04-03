9a78f077038e1f05527318a7a14e2ac77043d1bf
deliballiomer <49181185+deliballiomer@users.noreply.github.com>
2019-04-03 08:44:42 +0300

    Add files via upload

---
+++ displacementNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 0, sayi2 = 0;
+            Console.Write("Sayı1'i giriniz:");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sayı2'yi giriniz:");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            sayi1 = sayi1 + sayi2;
+            sayi2 = sayi1 - sayi2;
+            sayi1 = sayi1 - sayi2;
+            Console.WriteLine("Değişkenleri değiştirilen sayıların değeri: \nsayi1= {0}   sayi2=  {1}", sayi1, sayi2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
